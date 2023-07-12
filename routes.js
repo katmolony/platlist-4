e9e53759eaf829d5c6357dd1dd0c8495289b8fa8
Kate Molony <katemolony96@gmail.com>
2023-07-12 17:10:06 +0100

    Edit track feature

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { playlistController } from "./controllers/playlist-controller.js";
 import { aboutController } from "./controllers/about-controller.js";
+import { trackController } from "./controllers/track-controller.js";
 
 export const router = express.Router();
 
@@ -19,4 +20,8 @@ router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:id", playlistController.index);
 router.post("/playlist/:id/addtrack", playlistController.addTrack);
 router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
+
+router.get("/playlist/:playlistid/edittrack/:trackid", trackController.index);
+router.post("/playlist/:playlistid/updatetrack/:trackid", trackController.update);
+
 router.get("/about", aboutController.index);
